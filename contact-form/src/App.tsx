280a1f31063fef00457ca55150add92c334244c4
Artsiom Kukankou <147983587+ypayel@users.noreply.github.com>
2025-02-01 13:34:02 +0100

    create new app

---
+++ contact-form/src/App.tsx
@@ -0,0 +1,12 @@
+
+import './App.scss';
+import React from "react";
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ contact-form/src/components/ForumMain/ForumMain.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+export const ForumMain = () => {
+    return (
+        <>
+        <div className="main-conteiner">
+                <h2 className="first-header">Contact us</h2>
+                <div className="first-name-conteiner">
+                    <div className="first-name-conteiner">
+                    <h3 className="first-name-header">First Name</h3>
+                    </div>
+                </div>
+        </div>
+        </>
+    )
+}
+export default ForumMain; 
