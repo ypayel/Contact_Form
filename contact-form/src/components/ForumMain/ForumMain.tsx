2aebf1814d203eee84ceaea80813940f7b256099
Artsiom Kukankou <147983587+ypayel@users.noreply.github.com>
2025-02-03 12:59:18 +0100

    update

--- contact-form/src/components/ForumMain/ForumMain.tsx
+++ contact-form/src/components/ForumMain/ForumMain.tsx
@@ -1,4 +1,3 @@
-
 import React, { useState } from "react";
 import "./ForumMain.scss";
 export const ForumMain = () => {
@@ -10,67 +9,78 @@ export const ForumMain = () => {
         <form action="form" className="form-contact">
           <h2 className="first-header">Contact us</h2>
           <div className="name-main">
-            <div className="first-name-conteiner">
-              <h3 className="first-name-header">First Name <span>*</span></h3>
+            <div className="headers-conteiner">
+              <h3 className="first-name-header">
+                First Name <span>*</span>
+              </h3>
+              <h3 className="last-name-header">
+                Last Name <span>*</span>
+              </h3>
             </div>
-            <div className="first-input-conteiner">
+            <div className="name-inputs-conteiner">
               <input type="text" className="first-input" alt="first-name" />
-            </div>
-          </div>
-          <div className="last-main">
-            <div className="last-name-conteiner">
-              <h3 className="last-name-header">Last Name <span>*</span></h3>
-            </div>
-            <div className="last-input-conteiner">
+              <div></div>
               <input type="text" className="last-input" alt="last-name" />
             </div>
           </div>
           <div className="email-main">
-            <div className="email-holder">
-              <h3 className="email-header">Email Adress <span>*</span></h3>
+            <div className="email-header-conteiner">
+              <h3 className="email-header">
+                Email Adress <span>*</span>
+              </h3>
             </div>
             <div className="email-input-conteiner">
               <input type="text" className="email-input" alt="email" />
             </div>
           </div>
           <div className="general-query-conteiner">
-            
-              <h3 className="general-query-header">Query Type <span>*</span></h3>
-            
-            <div className="general-query-button">
-            <button 
-              type="button" 
-              className={`query-button ${queryType === "general" ? "active" : ""}`}
-              onClick={() => setQueryType("general")}
-            >
-              General Enquiry
-            </button>
-            <button 
-              type="button" 
-              className={`query-button ${queryType === "support" ? "active" : ""}`}
-              onClick={() => setQueryType("support")}
-            >
-              Support Request
-            </button>
-          </div>
+            <h3 className="general-query-header">
+              Query Type <span>*</span>
+            </h3>
+            <div className="general-query-buttons">
+              <button
+                type="button"
+                className={`query-button ${
+                  queryType === "general" ? "active" : ""
+                }`}
+                onClick={() => setQueryType("general")}
+              >
+                General Enquiry
+              </button>
+              <button
+                type="button"
+                className={`query-button ${
+                  queryType === "support" ? "active" : ""
+                }`}
+                onClick={() => setQueryType("support")}
+              >
+                Support Request
+              </button>
+            </div>
           </div>
           <div className="message-conteiner">
             <div className="message-holder">
-              <h3 className="message-header">Message <span>*</span></h3>
+              <h3 className="message-header">
+                Message <span>*</span>
+              </h3>
             </div>
             <div className="message-input-conteiner">
-                <input type="text" className="message-input" />
+              <input type="text" className="message-input" />
             </div>
           </div>
           <div className="consent-button-conteiner">
-            <button type="button"
-             className={`consent-button ${constentType === "accept" ? "active" : "unactive"}` }
-             onClick={() => setConstentType(constentType === `accept` ?  `` : `accept`)}
-
-              > 
-              
-              </button>
-             <span className="consent-text"> I consent to being contacted by the team <span>*</span></span>
+            <button
+              type="button"
+              className={`consent-button ${
+                constentType === "accept" ? "active" : "unactive"
+              }`}
+              onClick={() =>
+                setConstentType(constentType === `accept` ? `` : `accept`)
+              }
+            ></button>
+            <span className="consent-text">
+              I consent to being contacted by the team <span>*</span>
+            </span>
           </div>
           <div className="submit-button-conteiner">
             <button className="submit-button">Submit</button>
@@ -81,5 +91,3 @@ export const ForumMain = () => {
   );
 };
 export default ForumMain;
-
-
